53b976a19edca931b689ae5254dc55a1cfadba2e
Martin Machava <973475@slsp.sk>
2024-05-05 19:29:41 +0200

    TaskCards component init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+/*COMPONENTS:
+* 1.TasksCards
+* - delete task
+* - finished task (if you finish a task, you will be rewarded with a picture of an anime lady)
+* 3.EditTaskWindow
+* 2.AddTaskInput*/
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
