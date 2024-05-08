05002e0f70f4c0fd596c812326abe199fbf2946f
thaotic-good <fu3martina@gmail.com>
2024-05-08 18:58:51 +0200

    drop-down menu JavaScript

--- src/AddTask.tsx
+++ src/AddTask.tsx
@@ -1,25 +1,53 @@
 /*FUTURE USE CASE:
-* DeadlineDominatrix = 1. for each deadline you will be able to place a bet, If you meet your deadline you can place bet
-* on to the next deadline and see some titties, if you miss you'll lose it and the anime dominatrix will "spank" you
-* 2. betting game with friends to meet assignments*/
+* DeadlineDom lore: */
+import TasksCards from "./TasksCards";
+import React from "react";
 function AddTask() {
+    const [completeTask, setCompleteTask] =
+        React.useState({
+            id: "",
+            deadline: "",
+            task: "",
+            description: ""})
+    const [currentTask, setCurrentTask] = React.useState("")
+
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentTask(event.target.value)
+    }
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+    }
     return (
         <>
-            <form className="bg-red-800 rounded-lg m-5">
-            <label htmlFor="helper-text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Important deadline
-                </label>
-            <input type="text" id="helper-text" aria-describedby="helper-text-explanation"
-                   className="bg-gray-50 border border-blue-200 text-gray-900 text-sm rounded-lg mb-2 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                   placeholder="name@flowbite.com"/>
+            <TasksCards/>
+            <form  onSubmit={handleSubmit} className="shadow w-1/3 m-5">
+            <label htmlFor="helper-text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Insert task </label>
+                <div>
+                <input onChange={handleInput} type="text" id="helper-text" value={currentTask} aria-describedby="helper-text-explanation"
+                   className="bg-gray-50 border border-blue-200 text-gray-900 text-sm rounded-lg mb-2 focus:ring-blue-500
+                   focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+                   dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       placeholder="Deadline"/>
             <input type="email" id="helper-text" aria-describedby="helper-text-explanation"
                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg mb-2 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                       placeholder="name@flowbite.com"/>
-            <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-                Out of ideas?
+                       placeholder="Task"/>
+                    <input type="email" id="helper-text" aria-describedby="helper-text-explanation"
+                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg mb-2 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                           placeholder="Description"/>
+                </div>
+            <div className="flex justify-between items-center mt-8">
+                <button className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800
+                        focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Submit
+                </button>
+                <div className="flex gap-2">
+                <p id="helper-text-explanation" className="text-sm text-gray-500 dark:text-gray-400">
+                Out of ideas?</p>
                 <a href="https://mystudentvoices.com/what-are-good-habits-to-start-young-that-will-significantly-make-your-adult-life-easier-f6983c666280"
-                   className="font-medium text-blue-600 hover:underline dark:text-blue-500">
-                    Helpful guide</a>.</p>
+                   className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                    Helpful guide</a>
+                </div>
+                </div>
             </form>
         </>
     )
--- src/TasksCards.tsx
+++ src/TasksCards.tsx
@@ -1,63 +1,72 @@
 /*COMPONENT STRUCTURE:
-* 1.deadline
-* 2.task content
-* 3. button: Completed
-* 4. button: DOMINATED
+* 3. button: Crushed it = card will flip (ReactCardFlip), user will read a message, card will disappear
+* 4. button: Failed = card will flip, user will read a message, card will disappear
 * 5. drop: 1.edit 2.delete*/
 import React from "react";
-import axios from "axios"
 
-function TasksCards(){
-    const [tasks, setTasks] = React.useState([])
+function TasksCards() {
+    const [tasks, setTasks] = React.useState(["8.5.2024"])
+    const [showDropMenu, setShowDropMenu] = React.useState(false)
 
-    return(
+    const handleClickDropMenu = ()=>{
+        setShowDropMenu(!showDropMenu)
+    }
+
+    return (
         <>
-            {tasks.map(task =>
-                <div
-                    className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <div className="flex justify-end px-4 pt-4">
-                        <button id="dropdownButton" data-dropdown-toggle="dropdown"
-                                className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
-                                type="button">
-                            <span className="sr-only">Open dropdown</span>
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                 fill="currentColor" viewBox="0 0 16 3">
-                                <path
-                                    d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
-                            </svg>
-                        </button>
-                        {/* Dropdown menu */}
+            <div
+                className="w-full max-w-sm border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="bg-red-200 flex justify-end relative px-4 pt-4" onClick={handleClickDropMenu}>
+                    <button id="dropdownButton" data-dropdown-toggle="dropdown"
+                            className="bg-red-500 inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4
+                                 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+                            type="button">
+                        <span className="sr-only">Open dropdown</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                             fill="currentColor" viewBox="0 0 16 3">
+                            <path
+                                d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1
+                                0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
+                        </svg>
+                    </button>
+                    {showDropMenu &&
                         <div id="dropdown"
-                             className="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                             className="absolute top-50% right--200 z-10 text-base list-none bg-white divide-y divide-gray-100 rounded-lg
+                         shadow w-fit dark:bg-gray-700">
                             <ul className="py-2" aria-labelledby="dropdownButton">
                                 <li>
                                     <a href="#"
-                                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+                                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100
+                                   dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
                                 </li>
                                 <li>
                                     <a href="#"
-                                       className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+                                       className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100
+                                   dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
                                 </li>
                             </ul>
                         </div>
-                    </div>
-                    <div className="flex flex-col items-center pb-10">
-                        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Completed</h5>
-                        <span className="text-sm text-gray-500 dark:text-gray-400">DOMINATED</span>
-                        <div className="flex mt-4 md:mt-6">
-                            <a href="#"
-                               className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800
-                               focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-                                friend</a>
-                            <a href="#"
-                               className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200
+                    }
+                </div>
+                <div className="flex flex-col items-center pb-10">
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{tasks}</h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">...description of the deadline..</span>
+                    <div className="flex mt-4 md:mt-6">
+                        <a href="#"
+                           className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800
+                               focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crushed
+                            it</a>
+                        <a href="#"
+                           className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200
                                hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800
-                               dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Message</a>
-                        </div>
+                               dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Failed</a>
                     </div>
                 </div>
-            )}
+            </div>
+
+
         </>
     )
 }
+
 export default TasksCards
